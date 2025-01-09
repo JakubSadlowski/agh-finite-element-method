964cc59f4caf4ecdfd42a4a5fe1a86dbc844017d
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-01-09 15:19:23 +0100

    All is working properly.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         String filePath = "G:\\Programowanie_projekty\\IdeaProjects\\MES\\src\\main\\resources\\Test1_4_4.txt";
         String filePath2 = "G:\\Programowanie_projekty\\IdeaProjects\\MES\\src\\main\\resources\\Test2_4_4_MixGrid.txt";
-        GlobalData globalData = readFile(filePath2);
-        //printFromFile(globalData);
+        GlobalData globalData = readFile(filePath);
+        printFromFile(globalData);
         SolveEquation solveEquation = new SolveEquation(globalData);
         solveEquation.calculateResults(3);
     }
